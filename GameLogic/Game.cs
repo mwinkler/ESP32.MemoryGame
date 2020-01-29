06872516a1109d70d655ddb0aee32e3c361ad19d
Matthias Winkler <mwinkler@gmx.ch>
2020-01-30 00:11:36 +0100

    Add project files.

---
+++ GameLogic/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace GameLogic
+{
+    public class Game
+    {
+        private enum Sate
+        {
+            DISPLAY_PATH,
+            PLAYER_INPUTS
+        }
+        
+        private readonly IHardware Hardware;
+        private readonly int LedCount;
+
+        // state
+        private ArrayList Solution;
+        private int PlayerPosition;
+
+        public Game(IHardware hardware, int ledCount)
+        {
+            Hardware = hardware;
+            LedCount = ledCount;
+
+            // register button change event
+            hardware.OnButtonStateChanged(OnButtonStateChanged);
+        }
+
+        private void OnButtonStateChanged(int index, bool state)
+        {
+            Console.WriteLine($"[ButtonStateChanged] index: {index}, state: {state}");
+        }
+
+        public void Start()
+        {
+            // reset
+            Solution = new ArrayList();
+            PlayerPosition = 0;
+        }
+    }
+}
---
+++ GameLogic/IHardware.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameLogic
+{
+    public delegate void ButtonTriggerEvent(int index, bool state);
+
+    public interface IHardware
+    {
+        void SetLed(int index, bool state);
+        void OnButtonStateChanged(ButtonTriggerEvent callback);
+    }
+}
